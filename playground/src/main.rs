e0f759ee1ff5f083f07a893d70efddbc2d9f88b0
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-10-30 14:23:16 +0900

    wip

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -4,9 +4,50 @@ mod result_writer;
 
 use crate::hardware_random::RandomGenerator;
 use calc_population::calc_population;
+use std::fs::File;
+
+const RANGE: usize = 2048;
+const SIZE: usize = 2048 * 2048usize;
+const MASK: u64 = 0x07_FF;
+fn hard_rnd() -> [usize; RANGE] {
+	println!("enter hard_rnd");
+	let mut accum: [usize; RANGE] = [0; RANGE];
+	let mut rnd = hardware_random::HardRnd::default();
+
+	let mut recent = rnd.get_random_u64() & MASK;
+
+	for i in 0..SIZE {
+		if (i & 0x0F_FF == 0) {
+			println!("{i}/{SIZE}")
+		}
+
+		let current = rnd.get_random_u64() & MASK;
+		accum[recent.abs_diff(current) as usize] += 1;
+		recent = current;
+	}
+
+	accum
+}
+use std::env;
+use std::path::PathBuf;
+
+fn build_path(file_name: &str) -> String {
+	const arch: &str = if cfg!(target_arch = "x86_64") {
+		"x86_64"
+	} else if cfg!(target_arch = "aarch64") {
+		"aarch64"
+	} else {
+		"unknown"
+	};
+
+	format!("./artifacts/{arch}_{file_name}")
+}
 
 fn main() {
-	let mut foo = hardware_random::HardRnd::default();
+	let file = File::create("./artifacts/population.tsv").unwrap();
+	result_writer::write_result(file, &calc_population(2048)).unwrap();
 
-	println!("{}", foo.get_random_u64())
+	let file = File::create(build_path("hard_rand.tsv")).unwrap();
+	let accum = hard_rnd();
+	result_writer::write_result(file, &hard_rnd()).unwrap();
 }
--- scripts/helper.py
+++ scripts/helper.py
@@ -1,5 +1,7 @@
-import polars as pl
 import math
+from typing import List
+
+import polars as pl
 
 
 def calc_mean(df: pl.DataFrame, diff: str = 'diff', count: str = 'count') -> float:
@@ -21,3 +23,17 @@ def calc_std_dev(df: pl.DataFrame, mean: float = None, diff: str = 'diff', count
 def summary(df: pl.DataFrame, diff: str = 'diff', count: str = 'count') -> dict[str, float]:
     mean = calc_mean(df, diff, count)
     return {'mean': mean, 'std_dev': calc_std_dev(df, mean, diff, count)}
+
+
+def calc_sample(rng: int) -> List[int]:
+    if rng <= 0:
+        raise ValueError("Invalid value")
+
+    list = [rng]
+    cnt = (rng - 1) * 2
+
+    for i in range(rng - 1):
+        list.append(cnt)
+        cnt -= 2
+
+    return list
