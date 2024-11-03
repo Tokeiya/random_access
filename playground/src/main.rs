af8939c49bbdc327c755f1ce119c13b87bb45b39
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-11-03 14:06:12 +0900

    wip

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -6,6 +6,7 @@ mod result_writer;
 mod sample_generator;
 mod xorshift_64;
 
+use crate::hardware_random::HardRnd;
 use crate::hardware_random::RandomGenerator;
 use calc_population::calc_population;
 use generate_result::GenerateResult;
@@ -15,6 +16,8 @@ use rand_xoshiro::Xoshiro256StarStar;
 use range_sizes::RangeSizes;
 use sample_generator::*;
 use std::fs::File;
+
+use rayon::prelude::*;
 
 fn build_path(range: RangeSizes, iteration: usize, file_name: &str) -> String {
 	const arch: &str = if cfg!(target_arch = "x86_64") {
@@ -29,10 +32,38 @@ fn build_path(range: RangeSizes, iteration: usize, file_name: &str) -> String {
 }
 
 fn main() {
-	let mut generator = TheoreticalGenerator;
-	let result = generator.generate(RangeSizes::Size256, 1);
-	let mut writer =
-		std::fs::File::create(build_path(RangeSizes::Size256, 1, "theoretical")).unwrap();
+	let size = vec![
+		(RangeSizes::Size16, 16384),
+		(RangeSizes::Size512, 16),
+		(RangeSizes::Size1_024, 4),
+		(RangeSizes::Size2_048, 1),
+		(RangeSizes::Size4_096, 1),
+	];
 
-	result_writer::write_result(&mut writer, &result).unwrap();
+	size.par_iter().for_each(|(rng, ite)| {
+		let mut theoretical = TheoreticalGenerator;
+		let mut xorshift64 = XorShift64Generator::new(11_350_246_256_191_930_912);
+		let mut hardware = HardwareGenerator::new();
+		let mut xorshiro = XorShiroSampleGenerator::new(1_977_334_479_820_102_579);
+
+		println!("range:{} size:{}", rng, ite);
+
+		println!("theoretical");
+		let result = theoretical.generate(*rng, *ite);
+		let mut writer = File::create(build_path(*rng, *ite, "theoretical")).unwrap();
+		result_writer::write_result(&mut writer, &result).unwrap();
+
+		println!("xorshift64");
+		let result = xorshift64.generate(*rng, *ite);
+		let mut writer = File::create(build_path(*rng, *ite, "xorshift64")).unwrap();
+
+		println!("xorshiro64");
+		let result = xorshiro.generate(*rng, *ite);
+		let mut writer = File::create(build_path(*rng, *ite, "xorshiro64")).unwrap();
+
+		println!("hardware");
+		let result = hardware.generate(*rng, *ite);
+		let mut writer = File::create(build_path(*rng, *ite, "hardware")).unwrap();
+		result_writer::write_result(&mut writer, &result).unwrap();
+	})
 }
--- playground/src/sample_generator.rs
+++ playground/src/sample_generator.rs
@@ -3,7 +3,7 @@ use super::generate_result::GenerateResult;
 use super::range_sizes::RangeSizes;
 use super::xorshift_64::Xorshift64;
 use crate::hardware_random::{HardRnd, RandomGenerator};
-use rand_core::RngCore;
+use rand_core::{RngCore, SeedableRng};
 use rand_xoshiro::Xoshiro256StarStar;
 
 pub trait SampleGenerator {
@@ -29,20 +29,21 @@ impl XorShift64Generator {
 impl SampleGenerator for XorShift64Generator {
 	fn generate(&mut self, range: RangeSizes, iteration: usize) -> Vec<GenerateResult> {
 		let mut data = Vec::with_capacity(iteration);
-		let ite = range.to_usize() * iteration;
+		let ite = range.to_usize() * range.to_usize() * iteration;
 
 		for _ in 0..range.to_usize() {
 			data.push(GenerateResult::new(0, 0));
 		}
 
-		let mut recent = self.0.next();
+		let mask = range.to_usize() - 1;
+		let mut recent = self.0.next() as usize & mask;
 
 		for _ in 0..ite {
-			let idx = self.0.next();
+			let idx = self.0.next() as usize & mask;
 			let diff = recent.abs_diff(idx);
 
-			data[idx as usize].inclement_count();
-			data[recent as usize].inclement_diff();
+			data[idx].inclement_count();
+			data[recent].inclement_diff();
 		}
 
 		data
@@ -51,22 +52,29 @@ impl SampleGenerator for XorShift64Generator {
 
 pub struct XorShiroSampleGenerator(Xoshiro256StarStar);
 
+impl XorShiroSampleGenerator {
+	pub fn new(seed: u64) -> Self {
+		XorShiroSampleGenerator(Xoshiro256StarStar::seed_from_u64(seed))
+	}
+}
+
 impl SampleGenerator for XorShiroSampleGenerator {
 	fn generate(&mut self, range: RangeSizes, iteration: usize) -> Vec<GenerateResult> {
 		let mut data = Vec::new();
-		let ite = range.to_usize() * iteration;
+		let ite = range.to_usize() * range.to_usize() * iteration;
 		for _ in 0..range.to_usize() {
 			data.push(GenerateResult::new(0, 0));
 		}
 
-		let mut recent = self.0.next_u64();
+		let mask = range.to_usize() - 1;
+		let mut recent = self.0.next_u64() as usize & mask;
 
 		for _ in 0..ite {
-			let idx = self.0.next_u64();
+			let idx = self.0.next_u64() as usize & mask;
 			let diff = recent.abs_diff(idx);
 
-			data[idx as usize].inclement_count();
-			data[recent as usize].inclement_diff();
+			data[idx].inclement_count();
+			data[diff].inclement_diff();
 			recent = idx;
 		}
 
@@ -76,22 +84,33 @@ impl SampleGenerator for XorShiroSampleGenerator {
 
 pub struct HardwareGenerator<T>(T);
 
+impl HardwareGenerator<HardRnd> {
+	pub fn new() -> Self {
+		HardwareGenerator(HardRnd::default())
+	}
+}
+
 impl<T: RandomGenerator> SampleGenerator for HardwareGenerator<T> {
 	fn generate(&mut self, range: RangeSizes, iteration: usize) -> Vec<GenerateResult> {
 		let mut data = Vec::new();
-		let ite = range.to_usize() * iteration;
+		let ite = range.to_usize() * range.to_usize() * iteration;
 		for _ in 0..range.to_usize() {
 			data.push(GenerateResult::new(0, 0));
 		}
 
-		let mut recent = self.0.get_random_u64();
+		let mask = range.to_usize() - 1;
+		let mut recent = self.0.get_random_u64() as usize & mask;
 
-		for _ in 0..ite {
-			let idx = self.0.get_random_u64();
+		for i in 0..ite {
+			if i & 0xFFFF == 0 {
+				println!("{i}/{ite}");
+			}
+
+			let idx = self.0.get_random_u64() as usize & mask;
 			let diff = recent.abs_diff(idx);
 
-			data[idx as usize].inclement_count();
-			data[recent as usize].inclement_diff();
+			data[idx].inclement_count();
+			data[diff].inclement_diff();
 			recent = idx;
 		}
 
